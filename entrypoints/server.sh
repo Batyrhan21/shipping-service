9d7183a4c9f1f7aed3b6f90b53c8ec302b59f989
Батырхан <baatyr3030@gmail.com>
2023-05-30 18:02:41 +0600

    little corrects

--- entrypoints/server.sh
+++ entrypoints/server.sh
@@ -2,13 +2,18 @@
 cd src
 echo "Создаю миграции..."
 python manage.py makemigrations
+
 echo "Отправляю миграции..."
 python manage.py migrate
+
 echo "Собираю статику..."
 python manage.py collectstatic --no-input
+
 echo "Выгружаю данные из файла csv"
 python manage.py unload_locations ../uszips.csv
+
 echo "Cron schedule start"
 python manage.py runcrons
+
 echo "Запускаю сервер..."
 gunicorn shipping.wsgi --bind 0.0.0.0:8000 --reload
